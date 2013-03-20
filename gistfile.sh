83967855ac0f50506d1a5e443c912356d7aaaf96
harbolkn <harbolkn@gmail.com>
2013-03-20 02:02:25 -0400

    gist file

--- gistfile.sh
+++ gistfile.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
- 
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
-  
-if [ "$GIT_COMMITTER_EMAIL" = "harbolkn@Optimus." ]
-then
-cn="harbolkn"
-cm="harbolkn@gmail.com"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "harbolkn@gmail.com" ]
-then
-an="harbolkn"
-am="harbolkn@gmail.com"
-fi
-
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+  if ["$GIT_COMMITTER_NAME" = "harbolkn" ];
+  then
+    GIT_COMMITTER_NAME = "harbolkn";
+    GIT_AUTHOR_NAME = "harbolkn";
+    GIT_COMMITER_EMAIL = "harbolkn@gmail.com";
+    GIT_AUTHOR_EMAIL = "harbolkn@gmail.com";
+    git commit-tree "$@";
+  else
+    git commit-tree "$@";
+  fi' HEAD
